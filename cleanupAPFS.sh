c04fd4a459b7dd6a88bd3e2f4cabd856432f50c5
Somlea Marcel Ioan <marcel@aelmos.ro>
2020-04-28 22:28:46 +0300

    Update cleanupAPFS.sh

--- cleanupAPFS.sh
+++ cleanupAPFS.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Run this as root
+#Set needed disk partition
+
+DISK="disk1s5"
 for i in $(diskutil ap listSnapshots /| grep -i xid|awk -F ":" '{print $2}'); 
    do echo $i; 
-   diskutil apfs deleteSnapshot disk1s1 -xid $i; 
+   diskutil apfs deleteSnapshot $DISK -xid $i; 
 done
