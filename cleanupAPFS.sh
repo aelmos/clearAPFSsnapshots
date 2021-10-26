cea26932403574a8cf266f4248b8959115ae6ab4
Somlea Marcel Ioan <marcel@aelmos.ro>
2021-10-26 13:46:25 +0300

    Update cleanupAPFS.sh

--- cleanupAPFS.sh
+++ cleanupAPFS.sh
@@ -7,3 +7,10 @@ for i in $(diskutil ap listSnapshots /| grep -i xid|awk -F ":" '{print $2}');
    do echo $i; 
    diskutil apfs deleteSnapshot $DISK -xid $i; 
 done
+
+tmutil disable localsnapshot
+$Delete also localsnapshots
+for SNAPSHOT in $(tmutil listlocalsnapshots / | egrep '[0-9]{4}\-[0-9]{2}\-[0-9]{2}\-[0-9]{6}' -o | tail -r); 
+do tmutil deletelocalsnapshots $SNAPSHOT; 
+done
+sudo tmutil enable localsnapshot
